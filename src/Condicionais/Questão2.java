c21ea0b2e19cc266246f10bc548a0024615620d7
rafaeldeje <102198489+rafaeldeje@users.noreply.github.com>
2022-09-24 12:14:07 -0300

    Update Questão2.java

--- src/Condicionais/Questão2.java
+++ src/Condicionais/Questão2.java
@@ -1,14 +1,15 @@
 package Condicionais;
 import java.util.Scanner;
 
-public class Questão2 {
-	public static void main(String[] args) {
-//2) Faça um Programa que peça um valor e
-//mostre na tela se o valor é positivo ou negativo
-	Scanner leitor = new Scanner(System.in);
+public class Ex2 {
+public static void main(String[] args) {
+/*2) Faça um Programa que peça um valor e
+mostre na tela se o valor é positivo ou negativo*/
+	
+	Scanner scanner = new Scanner(System.in);
 	int n1;
 	System.out.println("Digite aqui um número: ");
-	n1 = leitor.nextInt();
+	n1 = scanner.nextInt();
 	if (n1 > 0){
 		System.out.println("O numero é positivo!");
 	}else if (n1 < 0){
